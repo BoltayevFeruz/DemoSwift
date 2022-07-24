1a230f83269958c97f207473198051fa0eb134a9
BoltayevFeruz <boltayevferuz777@gmail.com>
2022-07-24 21:35:05 +0500

    Initial Commit

---
+++ DemoSwift/DemoSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoSwiftApp.swift
+//  DemoSwift
+//
+//  Created by Feruz Boltayev on 24.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct DemoSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
